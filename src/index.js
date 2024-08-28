4fc2be2970c09a9d215a5540ff275e898039d406
Nalone <37165693+nalone7@users.noreply.github.com>
2024-08-28 15:03:47 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.panshengpeng.so": dockerHub,
+  "quay.panshengpeng.so": "https://quay.io",
+  "gcr.panshengpeng.so": "https://gcr.io",
+  "k8s-gcr.panshengpeng.so": "https://k8s.gcr.io",
+  "k8s.panshengpeng.so": "https://registry.k8s.io",
+  "ghcr.panshengpeng.so": "https://ghcr.io",
+  "cloudsmith.panshengpeng.so": "https://docker.cloudsmith.io",
+  "ecr.panshengpeng.so": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
